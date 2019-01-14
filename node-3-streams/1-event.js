4ac783207f25533c32e7b7143d3c5fed8304586a
hari <hari.k.krishna4@gmail.com>
2019-01-14 04:28:56 -0700

    added the eventEmitter

---
+++ node-3-streams/1-event.js
@@ -0,0 +1,32 @@
+var eventEmitter = require('events').EventEmitter;
+
+var getResource = (c) => {
+    var e = new eventEmitter();
+    process.nextTick(() => {
+        var count = 0;
+        e.emit('start');
+        var t = setInterval(() => {
+            e.emit('data', ++count);
+            if (count === c) {
+                e.emit('end', count);
+                clearInterval(t)
+            }
+        }, 10);
+    });
+return e
+
+};
+
+var r = getResource(5);
+
+r.on('start', () => {
+    console.log('I have started');
+});
+
+
+r.on('data', (d) => {
+    console.log('   I received:' +d);
+});
+r.on('end', () => {
+    console.log('I have ended');
+});
