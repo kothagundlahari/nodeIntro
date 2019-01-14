7d745dc750529154e66853bce86d5516e0306dc4
hari <hari@haris-MacBook-Pro.local>
2019-01-14 02:33:37 -0700

    callback functions

---
+++ node-1-intro/3-callback.js
@@ -0,0 +1,31 @@
+let maxTime = 1000;
+
+const evenDoubler = (v, callback) => {
+    let waitTime = Math.floor(Math.random()* (maxTime+1));
+   if(v%2) {
+       setTimeout(() => {
+            callback(new Error('Odd Input'));
+       }, waitTime)
+   }else {
+       setTimeout(() => {
+           callback(null, v*2, waitTime)
+       }, waitTime)
+   }
+};
+
+const handleResults = (err, results, time) => {
+    if(err) {
+        console.log('Error' +err.message);
+    }else {
+        console.log(`The results are ${results} (${time} ms)`)
+    }
+};
+
+evenDoubler(2, handleResults);
+evenDoubler(1, handleResults);
+evenDoubler(4, handleResults);
+evenDoubler(3, handleResults);
+evenDoubler(10, handleResults);
+
+
+console.log('=========');
