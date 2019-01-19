9e283d74a612edfd4c7821259fe7770cb18212f0
hari <hari.k.krishna4@gmail.com>
2019-01-19 10:42:38 -0700

    local streams

---
+++ node-4-localSystem/1-proc-stream.js
@@ -0,0 +1,12 @@
+
+process.stdin.resume();
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function (data1) {
+   process.stdout.write('Data -> '+data1)
+});
+
+process.stdin.on('end', function () {
+   process.stderr.write('End\n');
+});
