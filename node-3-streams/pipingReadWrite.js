43f830f7288ec5970a9595f240a3aaaa0249195c
hari <hari.k.krishna4@gmail.com>
2019-01-19 07:41:38 -0700

    reading the stream and writing the compressed to a file

--- node-3-streams/pipingReadWrite.js
+++ node-3-streams/pipingReadWrite.js
@@ -1,6 +1,10 @@
 var request = require('request');
 var fs = require('fs');
 
+var zlib = require('zlib');
+
 s = request('https://duckduckgo.com/');
 
 s.pipe(fs.createWriteStream('duckduckgo.html'));
+
+s.pipe(zlib.createGzip()).pipe(fs.createWriteStream('duckduckgo.gz.html'));
